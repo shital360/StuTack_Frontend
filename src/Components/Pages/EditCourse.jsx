874f2af14032c490ac44de8875dcdc256767e233
shital360 <shitaldangol@gmail.com>
2026-01-28 17:59:26 +0545

    editcourse

---
+++ src/Components/Pages/EditCourse.jsx
@@ -0,0 +1,17 @@
+function EditCourse() {
+  return (
+    <div>
+      <h2>Edit Course</h2>
+
+      <input value="C101" disabled />
+      <input value="Mathematics" />
+      <input value="10" />
+      <input value="5" />
+      <input value="Mr. Sharma" />
+
+      <button>Update</button>
+    </div>
+  );
+}
+
+export default EditCourse;
